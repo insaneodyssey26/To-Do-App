7c060b7a715ef30a22575bc70586ca2468fb694f
Masum <hydrapikachu29@gmail.com>
2025-07-30 18:47:33 +0530

    Fixed status bar bug in homescreen

--- app/src/main/java/com/masum/todo/presentation/TodoScreen.kt
+++ app/src/main/java/com/masum/todo/presentation/TodoScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -91,8 +90,7 @@ fun TodoScreen(
 
     Scaffold(
         modifier = modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection)
-            .statusBarsPadding(),
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             LargeTopAppBar(
                 title = {
